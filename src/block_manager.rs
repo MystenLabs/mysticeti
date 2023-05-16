c19b3a0fac6387cb7a0e7cbdfaed396d04620e81
George Danezis <george@mystenlabs.com>
2023-05-16 12:11:06 +0100

    Added logic to determine certificates at decision rounds

--- src/block_manager.rs
+++ src/block_manager.rs
@@ -64,7 +64,7 @@ pub struct TransactionEntry {
     accept_votes: HashSet<Authority>,
 
     // TODO: Refactor this to (1) indicate if a node will never vote to accept
-    //       due to already having accepted something else, or (2) to support 
+    //       due to already having accepted something else, or (2) to support
     //       a fast unlock type of mechanism.
     reject_votes: HashMap<Authority, Vote>,
 }
@@ -298,8 +298,8 @@ impl BlockManager {
     /// block including previous blocks only "votes" for the first block it sees from a validator for a round and none
     /// other in the future. However the blocks are followed and includes included to ensure we otherwise get all the
     /// past history even through equivocating blocks.
-    /// 
-    /// TODO: THIS CODE PREDATES THE COMMIT RULE BASED ON 2f+1 CERTIFICTES BEING SEEN AT THE DECISION ROUND
+    ///
+    /// TODO: THIS CODE PREDATES THE COMMIT RULE BASED ON 2f+1 CERTIFICATES BEING SEEN AT THE DECISION ROUND
     /// SO WE NEED TO AUDIT AND ADAPT IT TO THE NEW SCHEME.
     pub fn get_blocks_consensus_committed(
         &self,
@@ -396,6 +396,135 @@ impl BlockManager {
         }
 
         result
+    }
+
+    /// Apply the consensus decision rule for a given leader round and decision round.
+    pub fn get_decision_round_certificates(
+        &self,
+        leader_round: RoundNumber,
+        leader_name: &Authority,
+        decision_round: RoundNumber,
+    ) -> HashMap<&BlockReference, HashMap<&BlockReference, HashSet<&Authority>>> {
+        assert!(leader_round < decision_round);
+
+        // The candidates blocks on which all subsequent blocks will vote.
+        // TODO: if more than one, tag the leader as byzantine.
+        let candidates: Option<Vec<_>> =
+            self.blocks_processed_by_round.get(&leader_round).map(|b| {
+                b.iter()
+                    .filter(|block_reference| block_reference.0 == *leader_name)
+                    .collect()
+            });
+
+        // if the list is empty exit early.
+        let Some(candidates) = candidates else {
+            return HashMap::new();
+        };
+
+        // Initialize the blocks to votes map
+        let mut blocks_to_votes: HashMap<
+            &BlockReference,
+            HashMap<&BlockReference, HashSet<&Authority>>,
+        > = HashMap::new();
+
+        // Stores the first seen candidate block for each block, this is the candidate block for
+        // which a correct block will vote if it does not already contain a vote for a candidate
+        // block. Reminder: a correct authority only emits one block, it links to its previous blocks
+        // and as a result will only vote for a single candidate block.
+        let mut first_seen_block: HashMap<&BlockReference, &BlockReference> = HashMap::new();
+
+        // A correct authority will emit only one block, but here we have to take into account the
+        // posibility that a byzantine authority proposed two different blocks for the same round it
+        // is a leader. So we initialize the blocks_to_votes map with all the candidate blocks.
+        for cand in &candidates {
+            // Each candidate block votes for itself
+            blocks_to_votes.insert(cand, HashMap::new());
+            blocks_to_votes
+                .get_mut(cand)
+                .unwrap()
+                .insert(&cand, [&cand.0].into_iter().collect());
+
+            // Set the first seen to itself
+            first_seen_block.insert(&cand, &cand);
+        }
+
+        // Now we walk the rounds from leader_round to decision_round, and collect votes associated with each block
+        for round in leader_round..=decision_round {
+            let blocks_in_later_round = self.blocks_processed_by_round.get(&round).unwrap();
+            for block_reference_in_later_round in blocks_in_later_round {
+                let later_block = self
+                    .blocks_processed
+                    .get(block_reference_in_later_round)
+                    .unwrap();
+
+                // We will gather all the votes linked from this later block in this map
+                let mut votes: HashMap<&BlockReference, HashSet<&Authority>> = HashMap::new();
+                let current_authority = &block_reference_in_later_round.0;
+                let mut has_voted = false;
+
+                for block_reference_in_earlier_round in later_block.get_includes() {
+                    // If the reference is not in blocks_to_vote continue
+                    if !blocks_to_votes.contains_key(block_reference_in_earlier_round) {
+                        continue;
+                    }
+
+                    // If the first_seen for this block does not exit set it to the first seen of this block
+                    if !first_seen_block.contains_key(block_reference_in_later_round) {
+                        // note: this is safe since we initialize all entires in blocks_to_votes to also have a first_seen_block
+                        first_seen_block.insert(
+                            block_reference_in_later_round,
+                            &first_seen_block[&block_reference_in_earlier_round],
+                        );
+                    }
+
+                    if let Some(past_votes) = blocks_to_votes.get(block_reference_in_earlier_round)
+                    {
+                        for (block, new_votes) in past_votes {
+                            // If the block is not in the votes map, add it
+                            if !votes.contains_key(block) {
+                                votes.insert(block, HashSet::new());
+                            }
+
+                            // merge the past votes into the votes map
+                            votes.get_mut(block).unwrap().extend(new_votes);
+
+                            // if the current authority is in the votes we set the has_voted flag to true
+                            has_voted |= new_votes.contains(current_authority);
+                        }
+                    }
+                }
+
+                // If the votes map is empty continue, this block links to no candidates
+                if votes.is_empty() {
+                    continue;
+                }
+
+                // if has_voted flag is false, add the current authority to the votes map
+                if !has_voted {
+                    let my_first_seen_block = first_seen_block[block_reference_in_later_round];
+                    // assert this is a candidate block
+                    assert!(candidates.contains(&my_first_seen_block));
+
+                    if !votes.contains_key(&my_first_seen_block) {
+                        votes.insert(&my_first_seen_block, HashSet::new());
+                    }
+                    votes
+                        .get_mut(&my_first_seen_block)
+                        .unwrap()
+                        .insert(current_authority);
+                }
+
+                // Update the blocks_to_votes map
+                blocks_to_votes.insert(&block_reference_in_later_round, votes);
+            }
+        }
+
+        // filter out only the decision round blocks
+        let decision_round_blocks: HashMap<_, _> = blocks_to_votes
+            .into_iter()
+            .filter(|(block, _)| block.1 == decision_round)
+            .collect();
+        decision_round_blocks
     }
 
     /// List authorities waiting on a particular block reference. These are the authorities

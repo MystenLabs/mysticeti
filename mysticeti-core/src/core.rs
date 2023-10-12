05a13bbe9d8a5c946f2b9fcd9a94844a497f6def
Andrey Chursin <andrey@mystenlabs.com>
2023-10-12 13:19:58 -0700

    Increase block cache size
    This is needed until we have an LRU cache

--- mysticeti-core/src/core.rs
+++ mysticeti-core/src/core.rs
@@ -346,7 +346,7 @@ impl<H: BlockHandler> Core<H> {
     }
 
     pub fn cleanup(&self) {
-        const RETAIN_BELOW_COMMIT_ROUNDS: RoundNumber = 12;
+        const RETAIN_BELOW_COMMIT_ROUNDS: RoundNumber = 100;
 
         self.block_store.cleanup(
             self.last_commit_leader
